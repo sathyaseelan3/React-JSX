ff5fe22d0bb2c251075dcbf3a8a590dce90c9416
sathyaseelan3 <svseela3333@gmail.com>
2023-06-24 02:27:10 +0530

    React Jsx Msg

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PriceCard from "./components/price/PriceCard";
+
+function App() {
+    return (
+        <div className="App">
+            <PriceCard />
+        </div>
+    );
+
+}
+export default App;
